b84617ffc0259914ef8c3493738fe2abb6df68f2
Andrea Bulgarelli <bulgarelli@iasfbo.inaf.it>
2018-07-06 23:37:03 +0200

    spot6 install

--- install.sh
+++ install.sh
@@ -6,4 +6,6 @@ echo "install catalogs"
 test -d $AGILE/catalogs || mkdir -p $AGILE/catalogs
 cp -rf catalogs/* $AGILE/catalogs
 
-
+echo "install spot6"
+test -d $AGILE/AGILEPIPE/spot6 || mkdir -p $AGILE/AGILEPIPE/spot6
+cp -rf spot6/* $AGILE/AGILEPIPE/spot6
