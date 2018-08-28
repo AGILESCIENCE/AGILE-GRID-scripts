1d0b7c0fb8d1dce9393133c0e1ede5392048fdfd
Andrea Bulgarelli <bulgarelli@iasfbo.inaf.it>
2018-08-28 12:03:11 +0200

    removed cat2 scripts

--- cat2/extractcat.sh
+++
@@ -1,1 +0,0 @@
-extract_catalog.rb ~/results.agile/CAT2/catalog2/cat2b_3.multi $1 $2 list000A.multi 15 1 20 0 30 0
--- cat2/start.sh
+++
@@ -1,4 +0,0 @@
-ln -s ../FM3.119_ASDCe_I0023_B05.cts.gz
-ln -s ../FM3.119_ASDCe_I0023_B05.exp.gz
-ln -s ../FM3.119_ASDCe_I0023_B05.gas.gz
-cp ../FM3.119_ASDCe_I0023_B05.maplist4 .
