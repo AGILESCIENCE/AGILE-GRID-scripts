3c566a031ecc7175040a421f1a43725f7b8ae667
Andrea Bulgarelli <bulgarelli@iasfbo.inaf.it>
2018-08-29 16:29:31 +0200

    missing files

--- install.sh
+++ install.sh
@@ -9,4 +9,7 @@ cp -rf catalogs/* $AGILE/catalogs
 echo "install spot6"
 test -d $AGILE/AGILEPIPE/spot6 || mkdir -p $AGILE/AGILEPIPE/spot6
 cp -rf spot6/* $AGILE/AGILEPIPE/spot6
+cp -rf spot6/env.rb $AGILE/AGILEPIPE
+cp -rf spot6/template.ll $AGILE/AGILEPIPE
+
 
