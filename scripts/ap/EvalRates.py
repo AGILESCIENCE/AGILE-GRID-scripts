dc0ec3e66cac3ef41a81aec8034259827dc4d0ce
Andrea Bulgarelli <bulgarelli@iasfbo.inaf.it>
2018-09-11 00:15:49 +0200

    first ap version

---
+++ scripts/ap/EvalRates.py
@@ -0,0 +1,92 @@
+import string, os, sys
+import numpy as np
+import sys
+import math
+
+class EvalRates:
+
+    def __init__(self):
+        return
+
+    def getInstrumentPSF(self, selinstrumentID = 0, emin=100):
+        #AGILE
+        if instrumentID == 0:
+            if emin < 1700.: psf = 0.3 # deg.
+            if emin < 1000.: psf = 1. # deg.
+            if emin < 400.: psf = 3.5 # deg.
+            if emin < 100.: psf = 5.0 # deg. da provare
+
+        return psf
+
+    def calculateRateWithoutExp(self, verbose = 0, ranal= -1, fluxsource = 0e-08, gasvalue=-1, gal = 0.7, iso = 10., emin = 100., emax = 50000., instrumentID = 0):
+        fluxsource = float(fluxsource)
+        gasvalue = float(gasvalue) #andare direttamente nei file .disp.conv.sky.gz del modello e prendere il valore da li'
+        gal = float(gal)
+        iso = float(iso)
+        ranal = float(ranal)
+        emin = float(emin)
+        emax = float(emax)
+        if verbose == 1:
+            print('gasvalue %d', gasvalue)
+            print('gascoeff %d', gal)
+            print('isocoeff %d', iso)
+            print('emin %d', emin)
+            print('emax %d', emax)
+        psf = self.getInstrumentPSF(instrumentID, emin)
+        if verbose == 1:
+            print('selected psf: ' + str(psf))
+            print('selected ranal: ' + str(ranal))
+
+        #to take into account the extension of the PSF
+        fluxscalefactor = math.fabs(1-2*norm(0,  psf).cdf(ranal))
+
+        if verbose == 1:
+            print('fluxscalefactor based on PSF: ' + str(fluxscalefactor))
+
+        # ON - PSF region
+        omega_ranal = 2.*np.pi*(1. - np.cos(ranal*(np.pi/180.))) #[sr]
+        #AC = Andrew
+        omega_ranalAC = (np.pi/180. * ranal)**2 * np.sin(np.pi/180. * 30) / (np.pi/180. * 30) #[sr]
+
+        if verbose == 1:
+            print('[sr]   - ' + str(omega_ranal))
+            print('[sr] AC - ' + str(omega_ranalAC))
+
+        bkgdata = gasvalue*gal + iso*(10.**(-5)) # cts / [cm2 s sr]
+        bkg_ON = bkgdata*omega_ranal # [cts] / [cm^2 s sr] * [sr] = [cts] / [cm^2 s]
+
+        ctsgal = gasvalue*gal * omega_ranal #[cts] / [cm^2 s]
+        ctsiso = iso*(10.**(-5)) * omega_ranal #[cts] / [cm^2 s]
+
+        if verbose == 1:
+            print('--------------')
+            print('[cts] / [cm^2 s] - bkg_ON = ctsgal + ctsiso = ' + str(bkg_ON) + ' = ' + str(ctsgal) + ' + ' + str(ctsiso))
+
+        #B) flux source constant
+        fluxsource = fluxsource * fluxscalefactor #[cts / cm2 s]
+
+        #Calculation of counts
+        src_ON =  fluxsource #  [cts] / [cm^2 s]
+
+        ctstot = bkg_ON + src_ON # [cts] / [cm^2 s]
+
+        if verbose == 1:
+            print('[cts] / [cm^2 s] - src_ON = ' + str(src_ON))
+            print('[cts] / [cm^2 s] - ctstot (bkg + src) = ' + str(ctstot))
+            print('-------------- Moltiplica il valore sopra per exp in [cm2 s]')
+
+        return bkg_ON, src_ON
+
+    #exposure [cm2 s]
+    #fluxsource [cts] / [cm2 s]
+    #gasvalue [cts] / [cm2 s sr]
+    def calculateRate(self, verbose = 0, ranal= -1, exposure = 40000, fluxsource = 0e-08, gasvalue=-1, gal = 0.7, iso = 10., emin = 100., emax = 50000., instrumentID = 0):
+        bkg_ON, src_ON = calculateRateWithoutExp(verbose, ranal, fluxsource, gasvalue, gal, iso, emin, emax, instrumentID)
+        ctstot = (bkg_ON + src_ON) * exposure # [cts]
+        snr = src_ON * exposure / math.sqrt(float(ctstot))
+
+        print('src_ON = ' + str(src_ON))
+        print('bkg_ON = ' + str(bkg_ON))
+        print('ctstot = ' + str(ctstot))
+        print('SNR = ' + str(snr))
+        print('--------------')
---
+++ scripts/ap/GammaAP.py
@@ -0,0 +1,85 @@
+import scipy.signal as signal
+from astropy.stats import LombScargle
+import string, os, sys
+import numpy as np
+import sys
+import math
+import numpy as np
+import matplotlib.pyplot as plt
+
+class GammaAP:
+
+	def __init__(self):
+		return
+
+    def loadDataAPAGILE(apfile):
+        self.diml = 0
+        with open(apfile, "r") as ins:
+            for line in ins:
+                if(line != ""):
+                    val = line.split(" ")
+                    expdata = float(val[2])
+                    if(expdata != 0):
+                        self.diml += 1
+
+        self.tstartA = np.zeros(self.diml)
+        self.tstopA = np.zeros(self.diml)
+        self.expdataA = np.zeros(self.diml)
+        self.ctsdataA = np.zeros(self.diml)
+
+        nline = 0
+        with open(apfile, "r") as ins:
+            for line in ins:
+                if(line != ""):
+                    val = line.split(" ")
+                    tstart  = float(val[0])
+                    tstop   = float(val[1])
+                    expdata = float(val[2])
+                    ctsdata = float(val[3]) #select the right column
+                    if(expdata != 0):
+                        self.tstartA[nline] = tstart
+                        self.tstopA[nline] = tstop
+                        self.expdataA[nline] = expdata
+                        self.ctsdataA[nline] = ctsdata
+                        nline += 1
+
+    def normalizeAP(apfile):
+        return 0
+
+    def calculateLS():
+        scyp = 0
+        if scyp == 1:
+            nout = 100000
+            f = np.linspace(10e-9, 10e-4, nout)
+            pgram = signal.lombscargle(self.tstartA, self.ctsdataA, f)
+            plt.subplot(2, 1, 1)
+            plt.plot(tstartA, expdataA)
+            plt.subplot(2, 1, 2)
+            plt.gca().set_xscale("log")
+            plt.gca().set_yscale("log")
+            plt.plot(f, pgram)
+            plt.show()
+
+        if scyp == 0:
+            #normalization standard model log psd
+            ls = LombScargle(self.tstartA, self.ctsdataA, normalization='standard')
+            frequency, power = ls.autopower(minimum_frequency=1e-7, maximum_frequency=1e-4,samples_per_peak=100)
+
+            pls = ls.false_alarm_probability(power.max(), method='baluev')
+            ind = power.argmax()
+            maxf = frequency[ind]
+            daymax = 1 / maxf / 86400.
+            print('pls ' + str(pls) + ' with power of ' + str(power.max()) + ' at frequency ' + str(maxf) + ' Hz (' + str(daymax) + ') days' )
+
+            plt.subplot(1, 1, 1)
+            plt.plot(frequency, power)
+            plt.gca().set_xscale("log")
+            plt.gca().set_yscale("log")
+
+            #plt.subplot(3, 1, 2)
+            #best_frequency = frequency[np.argmax(power)]
+            #t_fit = np.linspace(0, 1)
+            #y_fit = LombScargle(tstartA, ctsdataA).model(t_fit, best_frequency)
+            #plt.plot(t_fit, y_fit)
+
+            plt.show()
