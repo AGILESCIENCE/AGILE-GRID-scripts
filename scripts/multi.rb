c3df90d88dd6101c1f41e685bf6e07098b178df3
Andrea Bulgarelli <bulgarelli@iasfbo.inaf.it>
2019-02-13 17:28:55 +0100

    correction in multi.rb help

--- scripts/multi.rb
+++ scripts/multi.rb
@@ -37,8 +37,9 @@
 #28) minimizerdefstrategy. Default 2 for Minuit
 #29) mindefaulttolerance. Defaul 0.01
 #xx) integratortype (1 gauss 2 gaussht 3 gausslefevre 4 gausslefevreht)
-#30) edpcorrection, default 0 (no), 1 yes. EDP cleaning correction NOT IMPLEMENTED YET
-#31) fluxcorrection, defaul 0 (no), 1 yes. Flux calculation correction for spectral shape in output - 2 input and output
+#FLUX CORRECTION:
+#30) edpcorrection, default 0.75, otherwise any value between 0 and 1. EDP correction is enabled only for E>1000 and if fluxcorrection=1. 
+#31) fluxcorrection. default 0 (no) - 1 yes. Flux calculation correction for spectral shape in output - 2 correction in input and output
 #32) scanmaplist - default 0. Calculate one TS for each map of the maplist4 provided as input, or group by some set of maps (e.g. for fovbinnumer > 1) -> specify the name of the source and the prefix e.g. VELA,pl -> one MLE using pl law for each map of the maplist4 for VELA source. e.g. VELA,pl,5 -> one MLE each 5 maps of the maplist4
 #33) (CAT) addcat. Specify the string of the source to be analysed". e.g. addcat="2.0e-07 34.7  -0.5  2.5 12 2 W44 0.0 1 2000.0 0.0". Remove sources with the same name, to avoid duplicate
 #34) (CAT) catpath, the path of the cat file list (.multi). Default is ENV["PATH_RES"] + /catalogs/cat2.multi
