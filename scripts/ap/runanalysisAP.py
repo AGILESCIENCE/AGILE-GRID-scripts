08e73d39039d991674f1b02ef837c5e85dfe6eb9
Andrea Bulgarelli <bulgarelli@iasfbo.inaf.it>
2019-08-31 15:43:22 +0200

    ranal as a parameter

--- scripts/ap/runanalysisAP.py
+++ scripts/ap/runanalysisAP.py
@@ -6,4 +6,5 @@ if __name__ == '__main__':
 	log_name_list =  sorted(glob.glob(regex), key=os.path.getmtime)
 	for filename in log_name_list:	
 		print(filename)
-		os.system("python analyzeAP.py " + filename)
+		ranal = filename.split("_")[2].split("R")[1]
+		os.system("python analyzeAP.py " + filename + " " + ranal)
