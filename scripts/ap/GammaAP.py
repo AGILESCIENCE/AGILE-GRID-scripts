cd5f23bfe079f446983a7a4788390395aca1b318
Andrea Bulgarelli <bulgarelli@iasfbo.inaf.it>
2019-09-02 19:04:50 +0200

    load ap3

--- scripts/ap/GammaAP.py
+++ scripts/ap/GammaAP.py
@@ -953,7 +953,7 @@ class GammaAP:
 			self.scanVM(apfilename + ".vm8.resgf", 8)
 
 	def fullAnalysisLoadAP3(self, ap3filename, ranal=2, gasvalue=0.00054, analyzevm=-1, vonmissesthread=48, freqmin=0.5e-06, freqmax=5.0e-06, vmnumax=100, ngridfreq=1000, tgridfreq=10800):
-		self.loadnormalizedAP3(ap2filename, ranal, gasvalue)
+		self.loadnormalizedAP3(ap3filename, ranal, gasvalue)
 		self.freqmin=float(freqmin)
 		self.freqmax=float(freqmax)
 		self.vmnumax=float(vmnumax)
